4993be8c737a275ea23bd2a1b46c16abcc96c12e
Ayush Tiwari <ayusht11@outlook.com>
2023-04-19 02:05:37 +0530

    update: deployment script

--- script/YearnPositionTWAPOracle.s.sol
+++ script/YearnPositionTWAPOracle.s.sol
@@ -12,24 +12,32 @@ contract YearnPositionTWAScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
+        string memory deploymentNetwork = vm.envString(
+            "DEPLOYMENT_NETWORK"
+        );
+
+        if (bytes(deploymentNetwork).length == 0) {
+            revert("Deployment network is not set in .env file");
+        }
+
         address curvePoolAddress = vm.envAddress(
-            string.concat("CURVE_POOL", deploymentNetwork)
+            string.concat("CURVE_POOL_", deploymentNetwork)
         );
 
         address yearnVaultAddress = vm.envAddress(
-            string.concat("YEARN_VAULT", deploymentNetwork)
+            string.concat("YEARN_VAULT_", deploymentNetwork)
         );
 
         address poolTokenAddress1 = vm.envAddress(
-            string.concat("POOL_TOKEN_1", deploymentNetwork)
+            string.concat("POOL_TOKEN_1_", deploymentNetwork)
         );
 
         address poolTokenAddress2 = vm.envAddress(
-            string.concat("POOL_TOKEN_2", deploymentNetwork)
+            string.concat("POOL_TOKEN_2_", deploymentNetwork)
         );
 
         address poolTokenAddress3 = vm.envAddress(
-            string.concat("POOL_TOKEN_3", deploymentNetwork)
+            string.concat("POOL_TOKEN_3_", deploymentNetwork)
         );
 
         address[3] memory coinPriceFeedAddresses = [
